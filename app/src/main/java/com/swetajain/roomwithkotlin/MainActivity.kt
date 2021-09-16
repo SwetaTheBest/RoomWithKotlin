52abed6f36dc63ba55f48558fc0e215bac38c4b0
Sweta Jain <sweta.jain@neosoftmail.com>
2021-09-16 21:26:51 +0530

    view binding and navigation main

--- app/src/main/java/com/swetajain/roomwithkotlin/MainActivity.kt
+++ app/src/main/java/com/swetajain/roomwithkotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.swetajain.roomwithkotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.swetajain.roomwithkotlin.databinding.ActivityMainBinding
 import java.sql.Timestamp
 import java.util.*
@@ -13,9 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHost
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
         val stamp = Timestamp(System.currentTimeMillis())
         val date = Date(stamp.time)
 //        val curTime = DateTime()
 //        curTime.dayOfWeek().getAsText(Locale.ENGLISH)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
